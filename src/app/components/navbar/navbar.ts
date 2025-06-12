5219be01fe340defd4e9494dfd9e4c8052bdd584
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-06-12 18:11:18 +0530

    fix: replace logout link with a styled button for better UX

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   private authService = inject(AuthService);
   isAuthenticated: Signal<boolean> = this.authService.isAuthenticated;
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
